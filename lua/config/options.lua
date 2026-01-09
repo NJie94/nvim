d76e325eb70c73d08d38a7871d0dc8d2d525b0e9
NicholasHi <kaijiehi@gmail.com>
2026-01-09 22:39:48 +0800

    Configure clipboard settings for SSH
    Set clipboard options based on SSH connection.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "telescope"
+
+-- Use system clipboard locally, but leave it unset over SSH so Neovim can use OSC52
+vim.opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus"
+
+
+if vim.env.SSH_CONNECTION then
+  vim.g.clipboard = "osc52"
+end
